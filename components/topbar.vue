0e5c1b60d79724eefc3dfd10184c8e837e54bdb1
Naphat <6431503016@lamduan.mfu.ac.th>
2023-11-17 02:47:05 +0700

    tidy up

--- components/topbar.vue
+++ components/topbar.vue
@@ -1,26 +1,29 @@
 <template>
   <!-- Single v-card section -->
   <v-card color="grey-lighten-4" flat height="70px" rounded="0">
-      <v-toolbar density="compact">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar density="compact">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-toolbar-title>EquipmentEase</v-toolbar-title>
+      <v-toolbar-title >
+        <router-link to="/" class="custom-link">EquipmentEase</router-link>
+      </v-toolbar-title>
 
-        <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-        <v-btn icon>
-          <v-icon>mdi-login</v-icon>
-        </v-btn>
-      </v-toolbar>
-      </v-card>
+      <v-btn icon>
+        <v-icon>mdi-login</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.custom-link {
+  color: black; /* Set the color to black or any other color you want */
+  text-decoration: none; /* Remove underline */
+}
 </style>
