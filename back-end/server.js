8fae25a11118875f610ef80ea5d3afec7c629ef3
NAPHAT CHAIYASANG <6431503016@lamduan.mfu.ac.th>
2023-11-21 13:57:37 +0700

    just in case 2

--- back-end/server.js
+++ back-end/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 8080;
+const port = 5001;
 
 app.use(bodyParser.json());
 app.use(cors());
